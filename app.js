3a1ba6af5d33b4760e7210c3b412bd0e7eea98ef
EleMint <dylanschmidt0216@gmail.com>
2018-06-14 15:41:29 -0500

    took out unused code

--- app.js
+++ app.js
@@ -130,7 +130,6 @@ function searchById(people)
 
 // Menu function to call once you find who you are looking for
 function mainMenu(person, people){
-  let linebreak = document.createElement("br");
   /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
   if(!person){
     alert("Could not find that individual.");
